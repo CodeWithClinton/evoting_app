66b62dc5b6152cd6475a4ec3782fccd2b2b0caa6
Clinton Nwachukwu <Clintonmatics@gmail.com>
2023-09-17 04:09:46 +0100

    pushing to render

--- myvoting/settings.py
+++ myvoting/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g=jzhv5zv8nxar2z0jpv#jv-$o*p(+e597*wgkau$gc&7v*m4e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'my-voting-app-q6ef.onrender.com']
 
 
 # Application definition
